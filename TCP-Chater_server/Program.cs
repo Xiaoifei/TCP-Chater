4ab8defc25d9f8167840f1b55a41ced86b855024
Xiaoifei <starifei@gmail.com>
2024-05-31 14:08:16 +0800

    update server

--- TCP-Chater_server/Program.cs
+++ TCP-Chater_server/Program.cs
@@ -6,67 +6,120 @@ namespace TCP_Chater;
 
 class Program
 {
-private static Socket socketClient;
     private static bool isClose = false;
+    private static Socket socket;
+    private static List<Socket> socketClientsList = new List<Socket>();
+    private static int socketClientNum = 0;
     private static void Main(string[] args)
     {
         Console.WriteLine("输入 command:Quit 即可退出程序");
-        // 1. 创建套接字Socket
-        Socket socketTcp = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-        // 2. 用Bind方法将套接字与本地地址绑定
+        Console.WriteLine("输入 command:Switch 可切换对象");
+        socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         try
         {
             IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
-            socketTcp.Bind(ipEndPoint);
+            socket.Bind(ipEndPoint);
         }
         catch (Exception e)
         {
-            Console.WriteLine("绑定失败"+e.Message);
+            Console.WriteLine("绑定失败");
+            Console.WriteLine(e.Message);
             throw;
         }
-        // 3. 用Listen方法监听
-        socketTcp.Listen(1);
+
+        socket.Listen(1024);
         Console.WriteLine("等待客户端连入");
-        // 4. 用Accept方法等待客户端连接
-        // 5. 建立连接，Accept返回新套接字
-        socketClient = socketTcp.Accept();
-        Console.WriteLine("连接建立成功！");
-        // 6. 用Send和Receive相关方法收发数据
+
+        Thread threadAcceptConnect = new Thread(AcceptClientConnect);
+        threadAcceptConnect.Start();
+        threadAcceptConnect.IsBackground = true;
+
         Thread threadReceiveMsg = new Thread(ReceiveMsg);
         threadReceiveMsg.Start();
-        socketClient.Send(Encoding.UTF8.GetBytes("Welcome to Connect to Server!"));
-        string sendMsg = Console.ReadLine();
+        threadReceiveMsg.IsBackground = true;
+
         while (!isClose)
         {
-            if (sendMsg == "command:Quit")
+            string sendMsg = Console.ReadLine();
+            if (string.IsNullOrEmpty(sendMsg)) continue;
+            if (sendMsg.StartsWith("command:"))
             {
-                isClose = true;
-                break;
+                sendMsg = sendMsg.Substring(8);
+                if (sendMsg == "Quit")
+                {
+                    Console.WriteLine("closing...");
+                    isClose = true;
+                    break;
+                }
+                else if (sendMsg == "Switch")
+                {
+                    Console.WriteLine("选择聊天对象:(输入序号即可)");
+                    for (int i = 0; i < socketClientsList.Count; i++)
+                    {
+                        Console.WriteLine(socketClientsList[i].RemoteEndPoint.ToString() + " 序号:" + i);
+                    }
+                    string tempstr = Console.ReadLine();
+                    if (!string.IsNullOrEmpty(tempstr) && int.Parse(tempstr)>=0 && int.Parse(tempstr)<= socketClientsList.Count)
+                    {
+                        socketClientNum = int.Parse(tempstr);
+                        Console.WriteLine("已切换至："+socketClientNum);
+                    }
+                }
             }
-            socketClient.Send(Encoding.UTF8.GetBytes(sendMsg));
-            sendMsg = Console.ReadLine();
+            else
+            {
+                Console.WriteLine("发送给："+socketClientNum);
+                try
+                {
+                    socketClientsList[socketClientNum].Send(Encoding.UTF8.GetBytes(sendMsg));
+                }
+                catch (Exception e)
+                {
+                    Console.Write("发送失败，原因：");
+                    Console.WriteLine(e);
+                }
+            }
         }
-        //关闭线程
         if (isClose)
         {
             threadReceiveMsg.Join();
-        }        
-        // 7. 用Shutdown方法释放连接
-        socketClient.Shutdown(SocketShutdown.Both);
-        // 8. 关闭套接字
-        socketClient.Close();
+            //threadAcceptConnect.Join();
+            for (int i = 0; i < socketClientsList.Count; i++)
+            {
+                socketClientsList[i].Shutdown(SocketShutdown.Both);
+                socketClientsList[i].Close();
+            } 
+            socketClientsList.Clear();
+        }
         Console.WriteLine("服务端已关闭");
     }
     static void ReceiveMsg()
     {
         byte[] result = new byte[1024];
+        Socket socketClient;
         while (!isClose)
         {
-            if (socketClient.Available > 0)
+            for (int i = 0; i < socketClientsList.Count; i++)
             {
-                int receiveNum = socketClient.Receive(result);
-                Console.WriteLine("Received Message From {0}:{1}",socketClient.RemoteEndPoint.ToString(),Encoding.UTF8.GetString(result,0,receiveNum));
+                socketClient = socketClientsList[i];
+                if (socketClient != null && socketClient.Available > 0)
+                {
+                    int receiveNum = socketClient.Receive(result);
+                    Console.WriteLine("Received Message From {0}:{1}",socketClient.RemoteEndPoint.ToString(),Encoding.UTF8.GetString(result,0,receiveNum));
+                }
             }
+        }
+    }
+
+    static void AcceptClientConnect()
+    {
+        Socket socketClient;
+        while (!isClose)
+        {
+            socketClient = socket.Accept();
+            socketClientsList.Add(socketClient);
+            socketClient.Send(Encoding.UTF8.GetBytes("Welcome to Connect to Server!"));
+            Console.WriteLine("与 " +socketClient.RemoteEndPoint.ToString() + " 连接建立成功！");
         }
     }
 }
