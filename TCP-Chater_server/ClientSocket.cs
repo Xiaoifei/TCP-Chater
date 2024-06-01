a794b7f09c57f9a2ccbb37dbd876fe2612b7e2de
Xiaoifei <starifei@gmail.com>
2024-06-01 17:51:50 +0800

    commit3 2024/6/1

---
+++ TCP-Chater_server/ClientSocket.cs
@@ -0,0 +1,74 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace TCP_Chater;
+
+public class ClientSocket
+{
+    private static int CLINET_COUNTER = 0; 
+    private int clientID;
+    public Socket socket;
+
+    public ClientSocket(Socket socket)
+    {
+        CLINET_COUNTER++;
+        clientID = CLINET_COUNTER;
+        this.socket = socket;
+    }
+
+    public void Close()
+    {
+        if (socket != null)
+        {
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+            socket = null;
+        }
+    }
+
+    public void Send(String msg)
+    {
+        if (socket != null)
+        {
+            try
+            {
+                socket.Send(Encoding.UTF8.GetBytes(msg));
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+            catch (ObjectDisposedException e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+            
+        }
+    }
+
+    public void Received()
+    {
+        if (socket != null)
+        {
+            if (socket.Available > 0)
+            {
+                byte[] bytes = new byte[1024];
+                socket.Receive(bytes);
+                ThreadPool.QueueUserWorkItem(HandleMsg,Encoding.UTF8.GetString(bytes));
+            }
+        }
+    }
+
+    public void HandleMsg(Object obj)
+    {
+        String msg = obj as string;
+        Console.WriteLine(msg);
+    }
+}
---
+++ TCP-Chater_server/ServerSocket.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace TCP_Chater
+{
+    public class ServerSocket
+    {
+        private Socket serverSocket;
+        private List<ClientSocket> clientLists = new List<ClientSocket>();
+        private bool isRunning;
+
+        public void Start()
+        {
+            try
+            {
+                isRunning = true;
+                serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+                serverSocket.Bind(ipEndPoint);
+                serverSocket.Listen(10);
+                ThreadPool.QueueUserWorkItem(Accept, null);
+                ThreadPool.QueueUserWorkItem(Receive, null);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unable Initialize"+e.Message);
+                serverSocket.Close();
+            }
+        }
+
+        public void Send(string msg)
+        {
+            for (int i = 0; i < clientLists.Count; i++)
+            {
+                clientLists[i].Send(msg);
+            }
+        }
+
+        public void Accept(Object obj)
+        {
+            while (isRunning)
+            {
+                try
+                {
+                    Socket socket = serverSocket.Accept();
+                    ClientSocket clientSocket = new ClientSocket(socket);
+                    clientLists.Add(clientSocket);
+                    clientSocket.Send("Welcome to Server!");
+                }
+                catch (SocketException e)
+                {
+                    Console.WriteLine("Accpet Method is forced to Close");
+                    Console.WriteLine(e.Message);
+                }
+                catch (ObjectDisposedException e)
+                {
+                    Console.WriteLine("wa2");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("wa3");
+                }
+            }
+        }
+
+        public void Receive(Object obj)
+        {
+            while (isRunning)
+            {
+                for (int i = 0; i < clientLists.Count; i++)
+                {
+                    clientLists[i].Received();
+                }
+            }
+        }
+
+        public void Close()
+        {
+            isRunning = false;
+            for (int i = 0; i < clientLists.Count; i++)
+            {
+                clientLists[i].Close();
+            }
+            clientLists.Clear();
+            if (serverSocket != null)
+            {
+                serverSocket.Close();
+                serverSocket = null;
+            }
+        }
+    }
+}
